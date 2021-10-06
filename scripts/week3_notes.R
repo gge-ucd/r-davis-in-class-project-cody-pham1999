5903b2cba456f109d06b1c43f45af66f58cc5655
Cody Pham <cody.pham1999@gmail.com>
2021-10-06 14:04:47 -0700

    Reorganized files, created wk3 notes script

---
+++ scripts/week3_notes.R
@@ -0,0 +1,8 @@
+#Goals
+#using the read.csv() function to read in data
+#explore and subset dataframes
+#packages and base R vs the tidyverse
+
+#reading in data
+#can use tab while typing in filepath to get suggestions
+surveys <- read.csv("data/portal_data_joined.csv")
